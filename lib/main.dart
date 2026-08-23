b1b2b3f377c7d79a26bdf4e7e97a41e8dc56877e
Jorge Lopez <jlopez@Jorges-MacBook-Pro.local>
2026-08-22 22:56:37 -0600

    First commit
    add: home screen

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home()
+    )
+  );
+}
+
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green[800],
+        title: Text('My First App'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          "hello world!!",
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600] 
+          )
+          ),
+      ),
+      floatingActionButton: FloatingActionButton( 
+        onPressed: ()=>(), 
+        backgroundColor: Colors.green[350],
+        hoverColor: Colors.pink[800],
+        child: Text('click'),
+      ),
+    );
+  }
+}
