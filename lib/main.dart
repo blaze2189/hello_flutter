b4c3b4d862baebd133a076a91848e90f652d643b
Jorge Lopez <jlopez@Jorges-MacBook-Pro.local>
2026-08-25 19:29:50 -0600

    Expanded
    add: expanded with image

--- lib/main.dart
+++ lib/main.dart
@@ -19,11 +19,15 @@ class Home extends StatelessWidget{
       body: Row(
         children: <Widget>[
           Expanded(
+            flex: 5,           
+            child: Image.asset('assets/215167.jpg')
+            ),
+          Expanded(
             flex: 3,
             child: Container(
               padding: EdgeInsets.all(30.0),
               color: Colors.cyan,
-              child: Text("text1")
+              child: Text("1")
             )
           ),
           Expanded(
